0a083fabc91e32d74dca09bb1f4c1a3b6efb4409
Manuel Borghardt <manuel@debonet.de>
2025-11-27 21:45:48 +0100

    test

--- libGDX-Enviroment/core/src/main/java/de/hsbi/programmieren/testat/Main.java
+++ libGDX-Enviroment/core/src/main/java/de/hsbi/programmieren/testat/Main.java
@@ -10,6 +10,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class Main extends ApplicationAdapter {
+
+    System.out.println("Main gestartet");
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
