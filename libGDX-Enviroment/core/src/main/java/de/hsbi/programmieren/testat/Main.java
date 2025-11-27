89aadd14446e18425c1b253c44267132794c3d22
Manuel Borghardt <manuel@debonet.de>
2025-11-27 14:39:11 +0100

    Ersten Dokumentationen und libGDX installiert

---
+++ libGDX-Enviroment/core/src/main/java/de/hsbi/programmieren/testat/Main.java
@@ -0,0 +1,7 @@
+package de.hsbi.programmieren.testat;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
